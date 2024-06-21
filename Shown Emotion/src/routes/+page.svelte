a5f7ff0e59ef48482bb716b1bdbf01d27612acfd
havaschk <118019537+havaschk@users.noreply.github.com>
2024-06-21 16:40:52 +0200

    Erster Test Push

---
+++ Shown Emotion/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<h1>Test PUSH!!</h1>
+<!--Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation-->
+<script>
+    let isHappy = false;
+  
+    function toggleMood() {
+      isHappy = !isHappy;
+    }
+  </script>
+  
+  <button on:click={toggleMood}>
+    {#if isHappy}
+      😊 <!-- Happy face emoji -->
+    {:else}
+      🙂 <!-- Slightly smiling face emoji -->
+    {/if}
+  </button>
+  
+  <style>
+    button {
+      font-size: 2rem;
+      padding: 0.5rem 1rem;
+      cursor: pointer;
+    }
+  </style>
+  
