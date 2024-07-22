235a930740421691e55ed636c810ffcee2e36178
Estelle Schenker <es_sc@uni-bremen.de>
2024-07-22 13:44:56 +0200

    Basic emotion calculation and updating working

--- CalculateEmotion/src/lib/router.js
+++ CalculateEmotion/src/lib/router.js
@@ -1,17 +1,76 @@
-let emotion;                            //Stores current emotion, is exported
-let arousal = 4;                        //Arousal parameter, inputrange: -3 to 4 
-let valence = 4;                        //Valence parameter, inputrange: -3 to 4 
-const key = `${arousal},${valence}`;    //Produces key to acces switchstatement
+import { error } from "@sveltejs/kit";
+import { writable } from "svelte/store";
 
-//Determents current emotion depending on arousal and valence
+
+
+//Creates a Svelte Store for managing the emotion sub-pages 
+export const currentEmotion = writable(0); 
+
+
+
+/**
+ * @type {number} Saves the Arousal Value
+ */
+let arousal = 4;
+/**
+ * @type {number} Saves the Valence Value
+ */
+let valence = 4;
+
+
+
+/**
+ * @param {number} index Is the current value of the Svelte Store, Function uptdates current Store Index
+ */
+export function setEmotion(index) {
+  currentEmotion.set(index);
+}
+
+
+
+/**
+ * @param {number} newArousal !!!!!NOT TESTET YET!!!!!
+ * @param {number} newValence Should update arousal and valence within the allowed parameters
+ */
+export function updateAoursalValence(newArousal, newValence){
+  if (arousal > 4 || arousal < -4){
+    throw new Error("Invalid value for arousal");
+  }
+  else if (valence > 4 || valence < -4){
+    throw new Error("Invalid value for valence");
+  }
+  else{
+    arousal = newArousal;
+    valence = newValence;
+  }
+
+  //TODO: Testing
+}
+
+
+
+//Mabye usefull in the future, currently unused
+export function getArousal(){
+  return arousal;
+}
+
+export function getValence(){
+  return valence;
+}
+
+
+
+//Calculates the actual emotion based on the arousal and valence values and traverses a switchstatemt to uptade the emotion
+export function calculateEmotion(){
+  const key = `${arousal},${valence}`;
+
 switch (key) {
   case '4,4':
-    emotion = 'amused';
+    return setEmotion(4);
     break;
-  // TODO: Implement more cases
+  // TODO: Implement missing cases
   // ...
   default:
     throw new Error('No valid combination found');
 }
-
-export {emotion};
+}
--- CalculateEmotion/src/routes/+page.svelte
+++ CalculateEmotion/src/routes/+page.svelte
@@ -1,10 +1,57 @@
 <script>
-    //Importing current emotion from router.js
-    import { emotion } from "../lib/router.js";
+    import { onMount } from "svelte";
+    import { currentEmotion, setEmotion, calculateEmotion} from "../lib/router.js";
+    import Amused from "./amused/+page.svelte";
+    import Bored from "./bored/+page.svelte";
+    import Calm from "./calm/+page.svelte";
+    import Excited from "./excited/+page.svelte";
+    import Frustrated from "./frustrated/+page.svelte";
+    import Happy from "./happy/+page.svelte";
+    import Sad from "./sad/+page.svelte";
+    import Worried from "./worried/+page.svelte";
+
+    
+    //Calculates current Emotion and sets the Svelte Store to the needed index
+    //setEmotion(0);
+    calculateEmotion();
 
 
-    //Faulty solution below
-    //TODO: Fix so current emotion is displayed
+    /**
+    * @type {number} Subscribes to the Svelte store so it can be used
+    */
+    let storeIndex;
+    const unsubscribe = currentEmotion.subscribe(value => {
+        storeIndex = value;
+    });
+
+
+    //Allows fexibility
+    onMount(() => {
+        return () => {
+        unsubscribe();
+        };
+    });
 </script>
 
-<svelte:component this={import('../routes/amused/+page.svelte')} />
+
+<!--Displays current emotion
+TODO: Mabye change to switch statement-->
+{#if storeIndex === 0}
+  <Amused />
+{:else if storeIndex === 1}
+  <Bored />
+{:else if storeIndex === 2}
+  <Calm />
+{:else if storeIndex === 3}
+  <Excited />
+{:else if storeIndex === 4}
+  <Frustrated />
+{:else if storeIndex === 5}
+  <Happy />
+{:else if storeIndex === 6}
+  <Sad /> 
+{:else if storeIndex === 7}
+  <Worried />
+{:else}
+  <p>Emotion not found</p>
+{/if}
