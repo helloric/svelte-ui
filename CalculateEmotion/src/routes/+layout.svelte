d21d23141c8b7154f736fe5f346b008141b665c9
Estelle Schenker <es_sc@uni-bremen.de>
2024-08-15 15:11:48 +0200

    Added colors (still incomplete)

--- CalculateEmotion/src/routes/+layout.svelte
+++ CalculateEmotion/src/routes/+layout.svelte
@@ -13,7 +13,7 @@
   import { doSpeaking } from "$lib/faceanimation";
 
   //Calculates current Emotion and sets the Svelte Store to the needed index
-  setEmotion(0);
+  setEmotion(1);
   //calculateEmotion();
 
   /**
