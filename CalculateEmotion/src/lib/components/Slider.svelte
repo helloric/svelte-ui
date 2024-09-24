8e7a974869d1e21ff40cae9c25e3b01aefeae773
Tobias <tobias6@uni-bremen.de>
2024-09-24 10:41:52 +0200

    Document the slider

--- CalculateEmotion/src/lib/components/Slider.svelte
+++ CalculateEmotion/src/lib/components/Slider.svelte
@@ -1,28 +1,41 @@
 <script lang="ts">
 	import { createEventDispatcher, onMount } from "svelte";
 
+    /** The lowest value possible for the bar to select. */
     export let lowerRange: number;
+
+    /** The highest value possible for the bar to select. */
     export let upperRange: number;
+
+    /** The value that should be displayed for the background bar. */
     export let backgroundValue: number;
+
+    /** The default value that the slider should be set to. */
     export let defaultValue: number;
 
-    //let currentValue: number = defaultValue;
+    /** Is the slider currently being dragged? */
     let dragging: boolean = false;
 
+    /** The bar width. */
     const BAR_WIDTH = 400;
 
+    /** The dispatcher that broadcasts the dropoff-event. */
     const dispatch = createEventDispatcher();
 
+    /** Which position on the bar corresponds to the slider value? */
     const getDrawPos = (value: number) => {
         return ((value - lowerRange) / (upperRange - lowerRange)) * BAR_WIDTH;
     }
 
+    /** Which slider value corresponds to a positon on the bar? */
     const posToValue = (pos: number) => {
         return (pos / BAR_WIDTH) * (upperRange - lowerRange) + lowerRange;
     }
 
+    /** Called each time the slider is placed. Dispatches the value that the slider has at the new position. */
     const dropOff = () => dispatch('dropoff', defaultValue);
 
+    /** Moves the slider and updates the value, if being dragged. */
     const onDragged = (e: MouseEvent) => {
 
         if (!dragging)
